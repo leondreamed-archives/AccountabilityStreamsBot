f4a35f00c8dd0927d16cbc8296eac83c9c85dc95
Leon Si <leonzalion@gmail.com>
2021-06-22 21:53:34 -0400

    tabs are better than spaces

--- src/index.ts
+++ src/index.ts
@@ -21,102 +21,102 @@ let giftCardRevealed = false;
 const client = new Discord.Client();
 
 async function revealGiftCard() {
-  await updatesChannel.send(
-    `${user.toString()} hasn't been streaming video for 15 minutes. Gift card code: ${process
-      .env.GIFT_CARD_CODE!}`
-  );
-  giftCardRevealed = true;
+	await updatesChannel.send(
+		`${user.toString()} hasn't been streaming video for 15 minutes. Gift card code: ${process
+			.env.GIFT_CARD_CODE!}`
+	);
+	giftCardRevealed = true;
 }
 
 function doesUserHaveVideo() {
-  return (
-    user.voice.channel !== null &&
-    user.voice.channelID === process.env.VOICE_CHANNEL_ID &&
-    user.voice.selfVideo
-  );
+	return (
+		user.voice.channel !== null &&
+		user.voice.channelID === process.env.VOICE_CHANNEL_ID &&
+		user.voice.selfVideo
+	);
 }
 
 async function checkStreaming() {
-  // Don't perform any checks if the gift card has already been revealed
-  if (giftCardRevealed) {
-    console.info(
-      "The gift card has already been revealed, streaming check skipped."
-    );
-    return;
-  }
+	// Don't perform any checks if the gift card has already been revealed
+	if (giftCardRevealed) {
+		console.info(
+			"The gift card has already been revealed, streaming check skipped."
+		);
+		return;
+	}
 
-  // If it's before 8:30AM or past 9:30PM, return
-  const date = dayjs();
-  const hour = date.tz().hour();
-  const minute = date.tz().minute();
-  console.info(`Current hour: ${hour}; Current minute: ${minute}`);
+	// If it's before 8:30AM or past 9:30PM, return
+	const date = dayjs();
+	const hour = date.tz().hour();
+	const minute = date.tz().minute();
+	console.info(`Current hour: ${hour}; Current minute: ${minute}`);
 
-  // Before 8:30AM
-  if (hour * 60 + minute < 8 * 60 + 30) {
-    return;
-  }
+	// Before 8:30AM
+	if (hour * 60 + minute < 8 * 60 + 30) {
+		return;
+	}
 
-  // Past 9:30PM
-  if (hour * 60 + minute >= 21 * 60 + 30) {
-    return;
-  }
+	// Past 9:30PM
+	if (hour * 60 + minute >= 21 * 60 + 30) {
+		return;
+	}
 
-  if (!doesUserHaveVideo()) {
-    console.info("User is not streaming his video...");
-    if (timeout === null) {
-      timeout = setTimeout(async () => {
-        // If the user still hasn't started streaming video by 15 mins, then reveal the gift card
-        if (!doesUserHaveVideo()) {
-          await revealGiftCard();
-        }
-      }, 1000 * 60 * 15);
-      let message = await updatesChannel.send(
-        `${user.toString()} is not streaming his video...`
-      );
-      notStreamingMessage = message;
-    }
-  } else {
-    console.info("User is streaming his video and the timeout was cleared.");
-    if (timeout !== null) {
-      clearTimeout(timeout);
-    }
-    timeout = null;
-    await notStreamingMessage?.delete();
-    notStreamingMessage = null;
-  }
+	if (!doesUserHaveVideo()) {
+		console.info("User is not streaming his video...");
+		if (timeout === null) {
+			timeout = setTimeout(async () => {
+				// If the user still hasn't started streaming video by 15 mins, then reveal the gift card
+				if (!doesUserHaveVideo()) {
+					await revealGiftCard();
+				}
+			}, 1000 * 60 * 15);
+			let message = await updatesChannel.send(
+				`${user.toString()} is not streaming his video...`
+			);
+			notStreamingMessage = message;
+		}
+	} else {
+		console.info("User is streaming his video and the timeout was cleared.");
+		if (timeout !== null) {
+			clearTimeout(timeout);
+		}
+		timeout = null;
+		await notStreamingMessage?.delete();
+		notStreamingMessage = null;
+	}
 }
 
 client.on("ready", async () => {
-  console.log("Bot is ready.");
+	console.log("Bot is ready.");
 
-  console.info("Fetching guild...");
-  guild = await client.guilds.fetch(process.env.GUILD_ID!);
+	console.info("Fetching guild...");
+	guild = await client.guilds.fetch(process.env.GUILD_ID!);
 
-  console.info("Fetching user...");
-  user = await guild.members.fetch(process.env.USER_ID!);
+	console.info("Fetching user...");
+	user = await guild.members.fetch(process.env.USER_ID!);
 
-  console.info("Fetching updates channel...");
-  updatesChannel = (await client.channels.fetch(
-    process.env.UPDATES_CHANNEL_ID!
-  )) as Discord.TextChannel;
+	console.info("Fetching updates channel...");
+	updatesChannel = (await client.channels.fetch(
+		process.env.UPDATES_CHANNEL_ID!
+	)) as Discord.TextChannel;
 
-  console.info("Fetching study stream channel...");
-  voiceChannel = (await client.channels.fetch(
-    process.env.VOICE_CHANNEL_ID!
-  )) as Discord.VoiceChannel;
+	console.info("Fetching study stream channel...");
+	voiceChannel = (await client.channels.fetch(
+		process.env.VOICE_CHANNEL_ID!
+	)) as Discord.VoiceChannel;
 
-  // Every minute, check if the user is streaming
-  schedule.scheduleJob("* * * * *", () => {
-    checkStreaming();
-  });
+	// Every minute, check if the user is streaming
+	schedule.scheduleJob("* * * * *", () => {
+		checkStreaming();
+	});
 
-  checkStreaming();
+	checkStreaming();
 });
 
 client.on("voiceStateUpdate", async (oldState, newState) => {
-  if (oldState.member?.id !== process.env.USER_ID!) return;
+	if (oldState.member?.id !== process.env.USER_ID!) return;
 
-  checkStreaming();
+	checkStreaming();
 });
 
 client.login(process.env.BOT_TOKEN);
