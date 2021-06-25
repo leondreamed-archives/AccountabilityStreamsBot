5eb8f4b9dcb69daa8538bbd7ce03d36bf15c1f77
Leon Si <leonzalion@gmail.com>
2021-06-24 23:52:26 -0400

    successfully extracts the date from a sleep cycle screenshot

--- src/sleep-cycle.ts
+++ src/sleep-cycle.ts
@@ -4,6 +4,15 @@ import Discord from "discord.js";
 import { user } from "./user";
 import { revealGiftCard } from "./gift-card";
 import { scheduleTzJob } from "./schedule";
+import tesseract from "node-tesseract-ocr";
+import Jimp from "jimp";
+import fs from "fs";
+
+const tesseractConfig = {
+	lang: "eng",
+	oem: 1,
+	psm: 3,
+};
 
 let latestSleepCycleMessage: Discord.Message | null = null;
 
@@ -22,6 +31,9 @@ const userIds = [
 
 const sleepCycleScreenshotWidth = 1242;
 const sleepCycleScreenshotHeight = 2359;
+
+const topLeft = { x: 50, y: 100 };
+const bottomRight = { x: 1000, y: 250 };
 
 export function registerSleepCyclePlugin(client: Discord.Client) {
 	// Request the sleep cycle message at 8:30AM
@@ -49,16 +61,41 @@ export function registerSleepCyclePlugin(client: Discord.Client) {
 	});
 
 	client.on("message", async (message) => {
-		if (
-			message.content === sleepCycleText &&
-			userIds.includes(message.author.id) &&
-			message.attachments.size > 0
-		) {
+		if (userIds.includes(message.author.id) && message.attachments.size > 0) {
 			const attachment = message.attachments.first()!;
 			if (
 				attachment.height === sleepCycleScreenshotHeight &&
 				attachment.width === sleepCycleScreenshotWidth
 			) {
+				const image = await Jimp.read(attachment.url);
+				image.crop(50, 100, 950, 150);
+
+				// Finding the right-most white pixel of the image
+				let maxX = 0;
+				image.scan(
+					0,
+					0,
+					image.bitmap.width,
+					image.bitmap.height,
+					function (x, y, idx) {
+						const red = this.bitmap.data[idx + 0];
+						const green = this.bitmap.data[idx + 1];
+						const blue = this.bitmap.data[idx + 2];
+
+						if (red === 255 && green === 255 && blue === 255) {
+							if (x > maxX) {
+								maxX = x;
+							}
+						}
+					}
+				);
+				image.crop(maxX, 0, image.getWidth() - maxX, image.getHeight());
+
+				const imageBuffer = await image.getBufferAsync("image/jpeg");
+				fs.writeFileSync("image.jpg", imageBuffer);
+
+				const text = await tesseract.recognize(imageBuffer, tesseractConfig);
+				message.channel.send(`Date detected: ${text.trim()}`);
 				console.info("Sleep cycle screenshot detected.");
 				latestSleepCycleMessage = message;
 			}
